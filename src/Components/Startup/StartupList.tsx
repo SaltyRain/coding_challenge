01eed4ee0a12f1f17ec8c2a3cf4e13511ad32532
Timur Garipov <tbgaripov@gmail.com>
2022-05-16 14:50:41 +0200

    changes during session

--- src/Components/Startup/StartupList.tsx
+++ src/Components/Startup/StartupList.tsx
@@ -1,5 +1,25 @@
-import { Fragment, ReactElement } from "react";
+import { Fragment, ReactElement} from "react";
+import { Card, Typography,  } from '@mui/material';
+// import StartupListItem from './StartupListItem';
+import { StartupHttpService }  from '../../Http/Startup/Startup.http.service';
 
 export default function StartupList(): ReactElement {
-  return <Fragment></Fragment>;
+
+
+  const data = StartupHttpService.getStartups();;
+  return <Fragment>
+
+    {/* {
+      startups.map((startup, startup_idx) => {
+        console.log(startup)
+        return (
+          <Card key={startup_idx}>
+            <Typography variant={'h4'} component={'h3'}>Test</Typography>
+          
+          </Card>
+      )})
+    } */}
+
+
+  </Fragment>;
 }
---
+++ src/Components/Startup/StartupListItem.tsx
@@ -0,0 +1,11 @@
+import { Fragment, ReactElement } from "react";
+import { Card, Typography,  } from '@mui/material';
+
+export default function StartupListItem(): ReactElement {
+  return <Fragment>
+    <Card>
+      <Typography variant={'h3'} component={'h3'}></Typography>
+      
+    </Card>
+  </Fragment>;
+}
